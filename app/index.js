25b18739b57c73fd5e8f59152577de0766bf59eb
tjedi <teammarvel0@gmail.com>
2020-05-07 13:35:57 +0530

    Created essential router functions

--- app/index.js
+++ app/index.js
@@ -6,6 +6,8 @@
 var http = require('http');
 var url = require('url');
 var stringDecoder = require('string_decoder').StringDecoder;
+var helpers = require('lib/helpers');
+var handlers = require('lib/handlers');
 
 var httpServer = http.createServer(function(req, res){
 	mainServer(req, res);
@@ -20,13 +22,42 @@ mainServer = function(req, res){
 	var queryStringObject = parsedUrl.query;
 	var method = req.method;
 	var headers = req.headers;
+		var decoder = new StringDecoder('utf-8');
+	var buff = '';
+	req.on('data', function(data){
+		buff += decoder.write(data);
+	});
+	req.on('end', function(){
+		buff += decoder.end();
 
-	console.log(path);
-	console.log(queryStringObject);
-	console.log(method);
-	console.log(headers);
-}
+		var handler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+		var data = {
+			'path': trimmedPath,
+			'query': queryStringObject,
+			'headers': headers,
+			'method': method,
+			'payload': helpers.parseJsonToObject(buff)
+		};
+
+		handler(data, function(statusCode, returnData){
+			statusCode = helpers.checknum(statusCode, 200);
+
+			res.setHeader('content-type', 'html');
+			res.writeHead(statusCode);
+			res.end(dataString);
+
+			console.log("Sent status code:", statusCode);
+		});
+
+	};
+};
 
 httpServer.listen(3000, function(){
 	console.log("The server is listening on port 3000");
-})
+});
+
+var router = {
+	'notFound' : handlers.notFound,
+	'ping' : handlers.ping
+};
